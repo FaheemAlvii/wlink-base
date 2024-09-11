37dbbaa37ea4d39ab3b2aa915b69693954df9ecf
FaheemAlvii <148661058+FaheemAlvii@users.noreply.github.com>
2024-09-11 16:24:22 +0500

    Initial

---
+++ gts_whatsapp/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Whatsapp API Handler',
+    'author': 'WLink',
+    'license': 'LGPL-3',
+    'version': '17.0.1.0',
+    'depends': ['mail'],
+    'description':'static/description/index.html',
+    'data': [
+        'security/ir.model.access.csv',
+        'views/connections.xml',
+        'wizard/login_menu.xml',
+
+        'views/menu.xml'
+    ],
+    'installable': True,
+}
