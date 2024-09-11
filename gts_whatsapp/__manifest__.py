773b7c3b2a0383773179f8ffb01f89edcd584b90
FaheemAlvii <148661058+FaheemAlvii@users.noreply.github.com>
2024-09-11 19:19:07 +0500

    Update __manifest__.py

--- gts_whatsapp/__manifest__.py
+++ gts_whatsapp/__manifest__.py
@@ -4,7 +4,7 @@
     'license': 'LGPL-3',
     'version': '17.0.1.0',
     'depends': ['mail'],
-    'price': 20,
+    'price': 30,
     'website':'https://wlink.geektechsol.com',
     'currency': 'USD',
     'currency': 'usd',
